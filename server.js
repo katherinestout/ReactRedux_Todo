6b567cd13e1b79ce47e925577d0899cb83c02b02
katherinestout <kjs14@hood.edu>
2018-10-03 13:24:05 -0400

    mongo connected

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+//DB configs
+//Getting access to the database
+const db = require('./config/keys').mongoURI;
+
+//Connect to MongoDB thru mongoose
+mongoose.connect(db).then(() => console.log('MongoDB Connected!'))
+.catch(err => console.log(err));
 
 app.get('/', (req, res) => res.send('Hello'));
 
-const mongoose = require('mongoose');
 
-const bodyParser = require('body-parser');
 
 const port = process.env.PORT || 5000;
 
