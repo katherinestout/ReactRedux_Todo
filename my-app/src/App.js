524fcf34dbfb92ec326e25f078d2951ed7db1513
katherinestout <kjs14@hood.edu>
2018-10-03 14:48:18 -0400

    react redux

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+//import {connect} from 'react-redux';
+//import {Provider} from 'react-redux';
+
 import './App.css';
 
 import Center from './components/Center';
@@ -7,11 +10,13 @@ import Center from './components/Center';
 class App extends Component {
   render() {
     return (
+     
       <Router>
       <div className="App">
         <Route exact path="/" component ={Center}/>
       </div>
       </Router>
+      
     );
   }
 }
---
+++ my-app/src/store/reducer.js
@@ -0,0 +1,18 @@
+const initialState = {
+    age: 21
+};
+
+const reducer = (state =initialState , action) => {
+    const newState = {...state};
+
+    if (action.type === 'AGE_UP'){
+        newState.age++;
+    }
+    if(action.type === 'AGE_DOWN'){
+        newState.age--;
+    }
+
+    return newState;
+};
+
+export default reducer;
