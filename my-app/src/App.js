38092c9dc461b3c3c08c513089d918b9c7555099
katherinestout <kjs14@hood.edu>
2018-10-03 14:29:43 -0400

    react

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
+
+import Center from './components/Center';
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
-        
+        <Route exact path="/" component ={Center}/>
       </div>
+      </Router>
     );
   }
 }
---
+++ my-app/src/components/Center.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+//import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+class Center extends Component {
+  render() {
+    return (
+     <div>
+         <h1>hi</h1>
+     </div>
+    );
+  }
+}
+
+export default Center;
